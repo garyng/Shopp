9dd44e284f0ade0aacfdd690d1c6b43d248fad17
GaryNg <garyng.zhongbo@gmail.com>
2018-11-29 20:49:57 +0800

    feat: add migration

---
+++ src/database-init/Shopp/Migrations/201811291248571_AddAllModels.cs
@@ -0,0 +1,115 @@
+namespace Shopp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddAllModels : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Admins",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserName = c.String(),
+                        Password = c.String(),
+                        Email = c.String(),
+                        Address = c.String(),
+                        Contact = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.OrderDetails",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ProductId = c.Int(nullable: false),
+                        Quantity = c.Int(nullable: false),
+                        PricePerUnit = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Subtotal = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        OrderId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Orders", t => t.OrderId, cascadeDelete: true)
+                .ForeignKey("dbo.Products", t => t.ProductId, cascadeDelete: true)
+                .Index(t => t.ProductId)
+                .Index(t => t.OrderId);
+            
+            CreateTable(
+                "dbo.Orders",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Total = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        UserId = c.Int(nullable: false),
+                        PaymentStatus = c.String(),
+                        PaymentChannel = c.String(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Users", t => t.UserId, cascadeDelete: true)
+                .Index(t => t.UserId);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserName = c.String(),
+                        Password = c.String(),
+                        Email = c.String(),
+                        Address = c.String(),
+                        Contact = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Price = c.Decimal(nullable: false, precision: 18, scale: 2),
+                        Category = c.String(),
+                        ImageUrl = c.String(),
+                        VendorId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Vendors", t => t.VendorId, cascadeDelete: true)
+                .Index(t => t.VendorId);
+            
+            CreateTable(
+                "dbo.Vendors",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        UserName = c.String(),
+                        Email = c.String(),
+                        Address = c.String(),
+                        Contact = c.String(),
+                        ShopName = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.OrderDetails", "ProductId", "dbo.Products");
+            DropForeignKey("dbo.Products", "VendorId", "dbo.Vendors");
+            DropForeignKey("dbo.OrderDetails", "OrderId", "dbo.Orders");
+            DropForeignKey("dbo.Orders", "UserId", "dbo.Users");
+            DropIndex("dbo.Products", new[] { "VendorId" });
+            DropIndex("dbo.Orders", new[] { "UserId" });
+            DropIndex("dbo.OrderDetails", new[] { "OrderId" });
+            DropIndex("dbo.OrderDetails", new[] { "ProductId" });
+            DropTable("dbo.Vendors");
+            DropTable("dbo.Products");
+            DropTable("dbo.Users");
+            DropTable("dbo.Orders");
+            DropTable("dbo.OrderDetails");
+            DropTable("dbo.Admins");
+        }
+    }
+}
